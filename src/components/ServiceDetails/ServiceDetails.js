940b0560b390fd0886997c2371cb805c63872c49
Morshed Kamal Nahin <86628012+Morshedkamalnahin@users.noreply.github.com>
2021-10-20 23:35:34 +0600

    Update ServiceDetails.js

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -19,7 +19,7 @@ const ServiceDetails = () => {
                 setServicsDetails(signleData)
             })
     }, [])
-    // const { img, name } = servicsDetails;
+    const { img, name } = servicsDetails;
 
 
     return (
@@ -28,10 +28,10 @@ const ServiceDetails = () => {
 
             <div className='container mx-auto d-flex justify-content-center align-items-center serviceDetails-container'>
                 <div className='w-100'>
-                    <img src={servicsDetails.img} className='serviceDetails-img' alt="" />
+                    <img src={img} className='serviceDetails-img' alt="" />
                 </div>
                 <div className='w-100 detailsContainer-info'>
-                    <h1>{servicsDetails.name}</h1>
+                    <h1>{name}</h1>
                     <p>In-network providers discount their services in order to access patients through vision-plan directories. Out-of-network providers set aside budgets to leverage various marketing channels. Whatever you’re doing to bring new patients into your practice or optical shop, it comes at a cost. The best marketing strategies for eye care providers bring those costs down over time.
 
                         That’s why some providers turn to content marketing to build their online presence. Creating content doesn’t require a capital investment—just time—and it’s an effective way to strengthen relationships with current and potential patients. When you publish content to engage website prospects, you’re generating interest at minimal cost while establishing yourself as a thought leader in your community.</p>
